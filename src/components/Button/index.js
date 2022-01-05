6d35ec0287815805e6072e1bff2bf282e07f0d0b
ifelipesilva <feehlipeeh422@gmail.com>
2022-01-05 13:03:45 -0300

    fix

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 //styles
 import { Wrapper } from './Button.Styles';
-import { string } from 'prop-types';
 
 const Button = ({ text, callback }) => (
   <Wrapper type="button" onClick={callback}>
